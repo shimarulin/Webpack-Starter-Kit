474f7e64baac5c19a5ba4f368e5722d7f546c461
Vyacheslav Shimarulin <shimarulin@gmail.com>
2016-10-09 21:24:06 +0300

    Init docker config

---
+++ export-aliases.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Export aliases to shell session"
+echo "You must use \"source\" command for call this script"
+alias npm='docker-compose run npm'
+alias webpack='docker-compose run webpack'
+alias wds='docker-compose run wds'
