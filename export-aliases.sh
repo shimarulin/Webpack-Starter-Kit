f35a8a9efc0a0d7a6f221ff65a250d9b06995a7f
Vyacheslav Shimarulin <shimarulin@gmail.com>
2016-10-10 03:26:35 +0300

    Serve static files with nginx-proxy

--- export-aliases.sh
+++ export-aliases.sh
@@ -3,5 +3,6 @@
 echo "Export aliases to shell session"
 echo "You must use \"source\" command for call this script"
 alias npm='docker-compose run npm'
+alias build='docker-compose run -e NODE_ENV=production webpack'
 alias webpack='docker-compose run webpack'
-alias wds='docker-compose run wds'
+alias wds='docker-compose up wds'
